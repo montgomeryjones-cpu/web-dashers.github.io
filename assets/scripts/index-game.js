08851f41dd5d926432aaa0fe4036eacaa2d809b2
PinkDev <gerbitravis@gmail.com>
2026-04-06 22:15:00 -0700

    percentage fix AGAIN

--- assets/scripts/index-game.js
+++ assets/scripts/index-game.js
@@ -3834,7 +3834,7 @@ class xs extends Phaser.Scene {
       this._leftBtn.setVisible(false)
       this._rightBtn.setVisible(false)
       this._percentageLabel.setVisible(true)
-      this._percentageLabel.setDepth(1000);
+      this._percentageLabel.setDepth(9999);
     }, () => this._menuActive && !this._playBtnPressed);
     this._positionMenuItems();
     this._spaceWasDown = false;
